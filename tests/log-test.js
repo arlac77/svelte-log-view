969241c5ea39307e006dd8e812c8ddee2fec618b
Markus Felten <Markus.Felten@gmx.de>
2020-06-10 01:47:50 +0200

    test: wip

--- tests/log-test.js
+++ tests/log-test.js
@@ -5,6 +5,7 @@ const base = "http://localhost:5000/base";
 fixture`log`.page`${base}/index.html`;
 
 test("several lines", async t => {
-  await t.expect(Selector('.log').innerText).eql('line 1\nline 2\nline 3\nline 4\nline 5\n');
+  await t.wait(5000);
+  await t.expect(Selector('.log').innerText).eql('line 1\nline 2\nline 3\nline 4\n');
 });
 
