840233e1f1aeccf3fbf140cffade92da559fe516
Markus Felten <Markus.Felten@gmx.de>
2020-06-11 19:17:33 +0200

    fix: export throttle

--- src/index.svelte
+++ src/index.svelte
@@ -1,5 +1,5 @@
 <script context="module">
   import LogView from './LogView.svelte';
-  import { lineIterator} from './lineIterator.mjs';
-  export { LogView, lineIterator };
+  import { lineIterator, throttle } from './lineIterator.mjs';
+  export { LogView, lineIterator, throttle };
 </script>
