f26c4f1cc4d6becb4b551009e2dafa18784089e8
Markus Felten <Markus.Felten@gmx.de>
2020-06-23 19:45:38 +0200

    style: reformat

--- example/rollup.config.mjs
+++ example/rollup.config.mjs
@@ -12,26 +12,33 @@ export default {
     format: "esm",
     file: "example/public/bundle.mjs"
   },
-  plugins: [svelte(), dev({
-    port,
-    dirs: ["example/public"],
-    spa: "example/public/index.html",
-    basePath: "/base",
-    extend(app, modules) {
-      app.use(
-        modules.router.get("/api/log", (ctx, next) => {
-          let i = 1;
-          ctx.body = new Readable({
-            encoding: "utf8",
-            read(size) {
-              setTimeout(() => this.push(`line ${i++}\n`), 500);
-            }
-          });
+  plugins: [
+    svelte(),
+    dev({
+      port,
+      dirs: ["example/public"],
+      spa: "example/public/index.html",
+      basePath: "/base",
+      extend(app, modules) {
+        app.use(
+          modules.router.get("/api/log", (ctx, next) => {
+            let i = 1;
+            ctx.body = new Readable({
+              encoding: "utf8",
+              read(size) {
+                setTimeout(() => this.push(`line ${i++}\n`), 500);
+              }
+            });
 
-          next();
-        })
-      );
-    }
-  }), resolve({ browser: true,
-            dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/') })]
+            next();
+          })
+        );
+      }
+    }),
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
+    })
+  ]
 };
