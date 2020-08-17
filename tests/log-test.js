5255ac73358dd4b1572ab0240d4ca11b976ba902
Markus Felten <Markus.Felten@db.com>
2020-08-17 12:21:57 +0200

    test: key down

--- tests/log-test.js
+++ tests/log-test.js
@@ -6,25 +6,33 @@ fixture`log`.page`${base}/index.html`;
 
 test("several lines", async t => {
   await t.wait(1000);
+  const log = Selector("#log");
+
   await t
     .expect(Selector("#log").innerText)
     .contains("line 1\nline 2\nline 3\nline 4\n");
 
   await t.expect(Selector("#follow").innerText).contains("F");
 
-  await t.pressKey("f");
+  await t.click(log).pressKey("f");
   await t.expect(Selector("#follow").innerText).contains("-");
 
-  await t.pressKey("f");
+  await t.click(log).pressKey("f");
   await t.expect(Selector("#follow").innerText).contains("F");
 
   await t.wait(2000);
 
-  await t.pressKey("g");
+  await t.click(log).pressKey("g");
   await t.wait(200);
   await t.expect(Selector("#start").innerText).contains("0");
 
-  await t.pressKey("G");
+  await t.click(log).pressKey("down");
+  await t.wait(200);
+  await t
+  .expect(Selector("#log").innerText)
+  .contains("line 1");
+
+  await t.click(log).pressKey("G");
   await t.wait(500);
   await t.expect(Selector("#start").innerText).contains("9");
 
