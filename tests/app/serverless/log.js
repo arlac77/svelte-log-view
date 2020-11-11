f13e992cb9bc59cddf417bdd7d04c68face0c3f6
Markus Felten <Markus.Felten@gmx.de>
2020-11-11 20:01:04 +0100

    test: check params

--- tests/app/serverless/log.js
+++ tests/app/serverless/log.js
@@ -1,7 +1,13 @@
-
 exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: "line 1"
-    };
+  const cursor =
+    event.queryStringParameters && event.queryStringParameters.cursor;
+  const offset =
+    event.queryStringParameters && event.queryStringParameters.offset;
+  const number =
+    event.queryStringParameters && event.queryStringParameters.number;
+
+  return {
+    statusCode: 200,
+    body: `${cursor}:${offset}:${number}\nline 1\nline 2\nline 3`
+  };
 };
