2fd69f32267bbc97893c37b762666a171f6ce7a3
Markus Felten <Markus.Felten@gmx.de>
2020-11-26 18:32:24 +0100

    chore(rollup): import postcss chore(rollup): add postcss

--- tests/app/rollup.config.mjs
+++ tests/app/rollup.config.mjs
@@ -1,3 +1,5 @@
+import postcss from "rollup-plugin-postcss";
+
 import virtual from "@rollup/plugin-virtual";
 import resolve from "@rollup/plugin-node-resolve";
 import dev from "rollup-plugin-dev";
@@ -15,59 +17,54 @@ export default {
     format: "esm",
     file: `${basedir}/public/bundle.main.mjs`
   },
-  plugins: [
-    virtual({
-      "node-fetch": "export default fetch",
-      stream: "export class Readable {}",
-      buffer: "export class Buffer {}"
-    }),
-    svelte(),
-    resolve({
-      browser: true,
-      dedupe: importee =>
-        importee === "svelte" || importee.startsWith("svelte/")
-    }),
-    dev({
-      port,
-      dirs: [`${basedir}/public`],
-      spa: `${basedir}/public/index.html`,
-      basePath: "/",
-      extend(app, modules) {
-        app.use(
-          modules.router.get("/api/log", (ctx, next) => {
-            const params = new URLSearchParams(
-              ctx.request.url.replace(/^[^\?]+\?/, "")
-            );
+  plugins: [virtual({
+    "node-fetch": "export default fetch",
+    stream: "export class Readable {}",
+    buffer: "export class Buffer {}"
+  }), svelte(), resolve({
+    browser: true,
+    dedupe: importee =>
+      importee === "svelte" || importee.startsWith("svelte/")
+  }), dev({
+    port,
+    dirs: [`${basedir}/public`],
+    spa: `${basedir}/public/index.html`,
+    basePath: "/",
+    extend(app, modules) {
+      app.use(
+        modules.router.get("/api/log", (ctx, next) => {
+          const params = new URLSearchParams(
+            ctx.request.url.replace(/^[^\?]+\?/, "")
+          );
 
-            let line = parseInt(params.get("cursor")) || 0;
-            const offset = parseInt(params.get("offset")) || 0;
-            const number = parseInt(params.get("number")) || 20;
+          let line = parseInt(params.get("cursor")) || 0;
+          const offset = parseInt(params.get("offset")) || 0;
+          const number = parseInt(params.get("number")) || 20;
 
-            line += offset;
+          line += offset;
 
-            let i = 0;
-            ctx.body = new Readable({
-              encoding: "utf8",
-              read(size) {
-                if (i++ < number) {
-                  setTimeout(() => this.push(`line ${line++}\n`), 80);
-                } else {
-                  console.log("size", size);
-                  try {
-                    this.push();
-                  } catch (e) {
-                    console.log(e);
-                  }
+          let i = 0;
+          ctx.body = new Readable({
+            encoding: "utf8",
+            read(size) {
+              if (i++ < number) {
+                setTimeout(() => this.push(`line ${line++}\n`), 80);
+              } else {
+                console.log("size", size);
+                try {
+                  this.push();
+                } catch (e) {
+                  console.log(e);
                 }
               }
-            });
+            }
+          });
 
-            //setTimeout(() => ctx.body.cancel(), 5000);
+          //setTimeout(() => ctx.body.cancel(), 5000);
 
-            next();
-          })
-        );
-      }
-    })
-  ]
+          next();
+        })
+      );
+    }
+  }), postcss()]
 };
