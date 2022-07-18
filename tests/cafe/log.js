f5420c58030aed67f49a36b7c47bbee61d80562c
Markus Felten <Markus.Felten@gmx.de>
2022-07-18 14:46:54 +0200

    test: fixtures

--- tests/cafe/log.js
+++ tests/cafe/log.js
@@ -17,8 +17,8 @@ test("several lines", async t => {
   await t.wait(100);
   
   await t.expect(Selector("#log").innerText).contains("line 1");
-  await t.expect(Selector("#start").innerText).contains("0");
-  await t.expect(Selector("#selected").innerText).contains("0");
+  await t.expect(Selector("#start").innerText).contains("line 0");
+  await t.expect(Selector("#selected").innerText).contains("line 0");
   await t.expect(Selector("#follow").innerText).contains("-");
 
   await t.click(log).pressKey("down");
@@ -27,7 +27,7 @@ test("several lines", async t => {
   await t.wait(100);
 
   await t.expect(Selector("#log").innerText).contains("line 3");
-  await t.expect(Selector("#start").innerText).contains("0");
+  await t.expect(Selector("#start").innerText).contains("line 1");
   await t.expect(Selector("#selected").innerText).contains("line 2");
   await t.expect(Selector("#follow").innerText).contains("-");
 
@@ -44,8 +44,8 @@ test("several lines", async t => {
   await t.wait(100);
   await t.click(log).pressKey("up");
   await t.wait(1000);
-  //await t.expect(Selector("#start").innerText).contains("0");
-  //await t.expect(Selector("#selected").innerText).contains("0");
+  await t.expect(Selector("#start").innerText).contains("line 1");
+  await t.expect(Selector("#selected").innerText).contains("line 1");
   await t.expect(Selector("#log").innerText).contains("line 1");
 
   await t.click(log).pressKey("f");
