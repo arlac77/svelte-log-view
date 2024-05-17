e9e66b0f99b114e1c24acd18caa1da105620a47c
Markus Felten <Markus.Felten@gmx.de>
2024-05-17 15:44:57 +0100

    test: be careful with clicking into log section

--- tests/cafe/log-cafe.mjs
+++ tests/cafe/log-cafe.mjs
@@ -6,14 +6,13 @@ fixture`log`.page`${base}`;
 
 test("several lines", async t => {
   await t.wait(100);
-  const log = Selector("#log");
 
   await t.expect(Selector("#log").innerText).contains("line 30\nline 31");
   //await t.expect(Selector("#start").innerText).contains("11");
   //await t.expect(Selector("#selected").innerText).contains("20");
   await t.expect(Selector("#follow").innerText).contains("F");
 
-  await t.click(log).pressKey("g");
+  await t.pressKey("g");
   await t.wait(100);
 
   await t.expect(Selector("#log").innerText).contains("line 0");
@@ -21,9 +20,9 @@ test("several lines", async t => {
   await t.expect(Selector("#selected").innerText).contains("0");
   await t.expect(Selector("#follow").innerText).contains("-");
 
-  await t.click(log).pressKey("down");
+  await t.pressKey("down");
   await t.wait(100);
-  await t.click(log).pressKey("down");
+  await t.pressKey("down");
   await t.wait(100);
 
   await t.expect(Selector("#log").innerText).contains("line 2");
@@ -31,31 +30,31 @@ test("several lines", async t => {
   await t.expect(Selector("#selected").innerText).contains("2");
   await t.expect(Selector("#follow").innerText).contains("-");
 
-  await t.click(log).pressKey("G");
+  await t.pressKey("G");
   await t.wait(500);
 
-  await t.expect(Selector("#log").innerText).contains("line 40");
+  await t.expect(Selector("#log").innerText).contains("line 39");
   //await t.expect(Selector("#start").innerText).contains("11");
   //await t.expect(Selector("#selected").innerText).contains("20");
   await t.expect(Selector("#follow").innerText).contains("-");
 
-  await t.click(log).pressKey("g");
+  await t.pressKey("g");
   await t.wait(100);
 
   /*
-  await t.click(log).pressKey("up");
+  await t.pressKey("up");
   await t.wait(1000);
   await t.expect(Selector("#start").innerText).contains("0");
   await t.expect(Selector("#selected").innerText).contains("0");
   await t.expect(Selector("#log").innerText).contains("line 0");
 
-  await t.click(log).pressKey("f");
+  await t.pressKey("f");
   await t.wait(2000);
 
   await t.expect(Selector("#log").innerText).contains("line 40");
   await t.expect(Selector("#follow").innerText).contains("F");
 
-  await t.click(log).pressKey("f");
+  await t.pressKey("f");
   await t.expect(Selector("#follow").innerText).contains("-");
   */
 });
